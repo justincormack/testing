d8b453673bad504ae6e951d6adccb79192927d27
Justin Cormack <justin@specialbusservice.com>
2015-04-25 17:29:43 +0100

    clean dir each time

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm -rf src
 cvs -d anoncvs@anoncvs.NetBSD.org:/cvsroot co -P src/sys/rump/listsrcdirs
 sh ./src/sys/rump/listsrcdirs -c all | xargs cvs -d anoncvs@anoncvs.netbsd.org:/cvsroot -z3 co -P
 git add -A src
